8d8a8a74944c5b20376a13a519462f93469454fe
Seth Ofosu Okyere <eskay.dev7@gmail.com>
2025-04-30 00:07:07 +0000

    Added Console utility class

---
+++ src/main/java/utilities/Console.java
@@ -0,0 +1,44 @@
+package utilities;
+
+import java.util.Scanner;
+
+public class Console {
+    public static Scanner input = new Scanner(System.in);
+
+    public static int readNumber(String promptMessage, int min, int max) {
+        int data = 0;
+        boolean isValidInput = false;
+        while (!isValidInput) {
+            System.out.print(promptMessage);
+            if (input.hasNextInt()) {
+                data = input.nextInt();
+                isValidInput = checkIfWithinRange(data, min, max);
+            } else {
+                System.out.println("ERROR, enter a whole number");
+                input.next();//use this to eat the invalid input in the scanner stream
+            }
+        }
+        return data;
+    }
+
+    public static String readString(String promptMessage) {
+        System.out.print(promptMessage);
+        String data = input.next();
+        return data;
+    }
+
+    public static String readLine(String promptMessage) {
+        System.out.print(promptMessage);
+        String data = input.nextLine();
+        System.out.println();
+        return data;
+    }
+
+    private static boolean checkIfWithinRange(int data, int min, int max) {
+        if (data < min || data > max) {
+            System.out.printf("ERROR, INPUT A VALUE BETWEEN %d and %d\n", min, max);
+            return false;
+        }
+        return true;
+    }
+}
